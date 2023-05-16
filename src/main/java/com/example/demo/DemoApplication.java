d777eed82561bc37b1719ba1d86af2747b3a0925
osamagdy <54372045+osamagdy@users.noreply.github.com>
2023-05-16 13:56:08 +0300

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Hello Hatabaaaa!, This is OpenShift";
+        return "Hello Hatabaaaa!!!, This is OpenShift";
     }
 
     public static void main(String[] args) {
